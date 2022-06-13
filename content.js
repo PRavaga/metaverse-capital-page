a62f34689f0ddca4a55b0a7848ff5b92fd1cdaca
jejolare <jejolare@vk.com>
2022-06-14 01:31:04 +0300

    mobile css fixes

--- content.js
+++ content.js
@@ -7,6 +7,7 @@ setTimeout(() => {
 }, 100);
 
 function caclPosition() {
+    if (window.innerWidth < 850) return document.querySelector('.main-wrapper > div:nth-child(3)').style.top = '300px'; 
     const defaultPosition = (window.innerHeight > 890 && window.innerWidth > 1200) ? -23.5 : -12;
     document.querySelector('.main-wrapper > div:nth-child(3)').style.top = 
     (document.querySelector("body > main > div > div:nth-child(2) > h2").getBoundingClientRect().top + defaultPosition) + 'px';
