a791c2dcdd1345100fc6f4b76df8fe88d82ec52c
jejolare <jejolare@vk.com>
2022-06-14 01:27:50 +0300

    text position fixes

--- content.js
+++ content.js
@@ -1,6 +1,13 @@
-const defaultPosition = (window.innerHeight > 890 && window.innerWidth > 1200) ? 46 : 48;
-document.querySelector('.main-wrapper > div:nth-child(3)').style.bottom = (defaultPosition + (window.innerHeight-850)/250) + '%';
 window.addEventListener('resize', () => {
-    const defaultPosition = (window.innerHeight > 890 && window.innerWidth > 1200) ? 46 : 48;
-    document.querySelector('.main-wrapper > div:nth-child(3)').style.bottom = (defaultPosition + (window.innerHeight-850)/250) + '%';
+    caclPosition();
 });
+caclPosition();
+setTimeout(() => {
+    caclPosition();
+}, 100);
+
+function caclPosition() {
+    const defaultPosition = (window.innerHeight > 890 && window.innerWidth > 1200) ? -23.5 : -12;
+    document.querySelector('.main-wrapper > div:nth-child(3)').style.top = 
+    (document.querySelector("body > main > div > div:nth-child(2) > h2").getBoundingClientRect().top + defaultPosition) + 'px';
+}
