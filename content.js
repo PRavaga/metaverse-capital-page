9817455366bad7714e80f69e6cad3665a5d68059
jejolare <jejolare@vk.com>
2022-06-15 12:29:52 +0300

    text position fix

--- content.js
+++ content.js
@@ -8,11 +8,11 @@ setTimeout(() => {
 
 function caclPosition() {
     if (window.innerWidth <= 850) return document.querySelector('.main-wrapper > div:nth-child(3)').style.top = '300px'; 
-    const h2Position = document.querySelector("body > main > div > div:nth-child(2) > h2").getBoundingClientRect().y;
-    const difference = document.querySelector("body > main > div > div:nth-child(3) > pre").getBoundingClientRect().y - document.querySelector("body > main > div > div:nth-child(3) > h2").getBoundingClientRect().y
-    const letterSpacing = parseFloat(getComputedStyle(document.querySelector('body > main > div > div:nth-child(3) h2')).letterSpacing, 10);
-    const resolutionIndex = window.innerWidth > 1500 ? (1920/window.innerWidth)*1.5 : (1920/window.innerWidth)*0.9;
-    const rightBlockPosition = h2Position + difference - letterSpacing*resolutionIndex;
+    const h2Position = document.querySelector("body > main > div > div:nth-child(2)").getBoundingClientRect().y;
+    // const difference = document.querySelector("body > main > div > div:nth-child(3) > pre").getBoundingClientRect().y - document.querySelector("body > main > div > div:nth-child(3) > h2").getBoundingClientRect().y
+    // const letterSpacing = parseFloat(getComputedStyle(document.querySelector('body > main > div > div:nth-child(3) h2')).letterSpacing, 10);
+    // const resolutionIndex = window.innerWidth > 1500 ? (1920/window.innerWidth)*1.5 : (1920/window.innerWidth)*0.9;
+    const rightBlockPosition = h2Position;
 
     document.querySelector('.main-wrapper > div:nth-child(3)').style.top = rightBlockPosition + 'px'
 }
